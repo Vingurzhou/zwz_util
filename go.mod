3afdbcb1c5bb7684fd0ff4cc9740aa81cc951119
vingurzhou <zwz0123460218@icloud.com>
2022-08-10 19:57:05 +0800

    test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zwz/zwz_util
+
+go 1.18
---
+++ test.go
@@ -0,0 +1,8 @@
+package zwz_util
+
+import "fmt"
+
+func Test() {
+	fmt.Println("hello world, this is my first publish module")
+	fmt.Println("this Hello func in top package")
+}
