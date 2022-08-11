c675f22150e8f7c9f4e6dfab92784dbe19d57d50
Vingurzhou <zwz0123460218@icloud.com>
2022-08-11 20:49:51 +0800

    Update test.go

--- test.go
+++ test.go
@@ -3,6 +3,5 @@ package zwz_util
 import "fmt"
 
 func Test() {
-	fmt.Println("hello world, this is my first publish module")
-	fmt.Println("this Hello func in top package")
+	fmt.Println("hello world")
 }
